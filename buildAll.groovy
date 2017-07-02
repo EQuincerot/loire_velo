db873181bbf5eb6d825a1b4e0fda16dcbb38a530
emmanuel <emmanuel>
2017-07-02 18:42:22 +0200

    Jour2 : Onzain à Tours (52km) Jour3 : Tours Chinon (101km) Jour4 : Brossay Bécon (87km) Jour5 : Bécon St Floren Le Viel (88km) Jour6 : Nantes Ste Margueurite (86km)
    buildAll.sh
    Amélioration de la sortie standard pour la rendre plus lisible
    Gestion d'un fichier qui est écrit à l'envers
    Détection du namespace du fichier (geovelo et eurovelo n'ont pas le même)

--- buildAll.groovy
+++ buildAll.groovy
@@ -1,22 +1,49 @@
 import groovy.util.XmlSlurper
 
+String guessNamespace(def parsedXml) {
+    String namespace = 'http://www.topografix.com/GPX/1/0'
+    if (parsedXml.declareNamespace(x: 'http://www.topografix.com/GPX/1/1').'x:trk'.'x:trkseg'.'x:trkpt'.size()) {
+        namespace = 'http://www.topografix.com/GPX/1/1'
+    }
+    return namespace
+}
+
+def getPoints(File file, def parsedXml, String namespace) {
+    def result = parsedXml.declareNamespace(x: namespace)
+            .'x:trk'.'x:trkseg'.'x:trkpt'
+    
+    return result
+            
+}
 
 int copyFile(def out, def file) {
     out.println "<!-- $file -->"
     int totalPoint = 0
     def parsedXml = new XmlSlurper().parseText(file.text)
-    def namespace = 'http://www.topografix.com/GPX/1/0'
-    if (parsedXml.declareNamespace(x: 'http://www.topografix.com/GPX/1/1').'x:trk'.'x:trkseg'.'x:trkpt'.size()) {
-        namespace = 'http://www.topografix.com/GPX/1/1'
+    
+    String namespace = guessNamespace(parsedXml)
+    def parsedXmlWithNs = parsedXml.declareNamespace(x: namespace)
+    
+    int trackCount = parsedXmlWithNs.'x:trk'.'x:trkseg'.size()
+    if (trackCount != 1) {
+        throw new RuntimeException("Found $trackCount tracks. One expected")
     }
-
-    parsedXml.declareNamespace(x: namespace)
-            .'x:trk'.'x:trkseg'.'x:trkpt'
-            .eachWithIndex { point, index ->
-        out.println """<trkpt lat="${point.'@lat'}" lon="${point.'@lon'}"><ele>0</ele></trkpt>"""
+    
+    String copy = ''
+    getPoints(file, parsedXml, namespace).eachWithIndex { point, index ->
+    
+        if (file.name.contains('62_Champtoceaux_MauvessurLoire_parcours')) {
+            // We need to inverse the order of the points in some files
+            copy = """<trkpt lat="${point.'@lat'}" lon="${point.'@lon'}"><ele>0</ele></trkpt>""" + copy
+        } else {
+            copy += """<trkpt lat="${point.'@lat'}" lon="${point.'@lon'}"><ele>0</ele></trkpt>"""
+        }
         totalPoint = index
     }
-    totalPoint
+    
+    out.println copy
+    
+    return totalPoint
 }
 
 void mergeTracks(int day, String newTrackName) {
@@ -39,13 +66,14 @@ void mergeTracks(int day, String newTrackName) {
 		<trkseg>
 """
 
+        println "${outputFileName} :"
         files.sort { it.name }.each {
-            print "${outputFileName} << Copie de ${it.name}"
+            print "\t\tCopie de ${it.name}"
             println " (${copyFile(out, it)} points)"
         }
 
 
-        out.println """\t\t</trkseg>
+        out.println """\t</trkseg>
 \t</trk>
 </gpx>
 """
@@ -54,4 +82,10 @@ void mergeTracks(int day, String newTrackName) {
 }
 
 mergeTracks(1, 'jour1_42-46_Aubrais-Onzain-via-Chambord')
+mergeTracks(2, 'jour2_47-49_Onzain-Tours')
+mergeTracks(3, 'jour3_50-53_Tours-Brossay-via-Saumur')
+mergeTracks(4, 'jour4_54-57-Brossay-Becon')
+mergeTracks(5, 'jour5_60-63-Becon-Nantes')
+mergeTracks(6, 'jour6_64-66-Nantes-StNazaire-viaPontStNazaire')
+
 'FIN' 
